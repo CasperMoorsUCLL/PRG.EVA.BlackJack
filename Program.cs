cdda0b7e7dc686916476421bacabc6050e2b652c
CasperMoorsUCLL <113981445+CasperMoorsUCLL@users.noreply.github.com>
2024-08-26 19:29:17 +0200

    commit3

--- Program.cs
+++ Program.cs
@@ -5,10 +5,56 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using PRG.EVA.BlackJack.Models;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+//// Voeg services toe aan de container
+//builder.Services.AddControllersWithViews();
+
+//var app = builder.Build();
+
+//// Configureer de HTTP-request pipeline
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//// Voeg specifieke routes toe voor de BlackJack controller
+//app.MapControllerRoute(
+//    name: "InitGame",
+//    pattern: "init/{bet:decimal}",
+//    defaults: new { controller = "BlackJack", action = "InitGame" });
+
+//app.MapControllerRoute(
+//    name: "PlayGame",
+//    pattern: "play/{option}",
+//    defaults: new { controller = "BlackJack", action = "Play" });
+
+//app.Run();
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Voeg de HttpClient service toe
+builder.Services.AddHttpClient();
 
+// Voeg de databasecontext toe
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 // Voeg services toe aan de container
@@ -34,7 +80,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Voeg specifieke routes toe voor de BlackJack controller
 app.MapControllerRoute(
     name: "InitGame",
     pattern: "init/{bet:decimal}",
@@ -46,3 +91,4 @@ app.MapControllerRoute(
     defaults: new { controller = "BlackJack", action = "Play" });
 
 app.Run();
+
